009ae5b2729743eda424c0779c230c778506b054
kreshnaBAP <kreshnabayu12@gmail.com>
2023-03-26 17:25:08 +0700

    fail, about to revert

--- jojo-react/app/src/pages/AddBagiOrderPage/TotalOrderTable.js
+++ jojo-react/app/src/pages/AddBagiOrderPage/TotalOrderTable.js
@@ -1,29 +1,160 @@
 import Loading from "../../components/Loading";
-import SelectSupplierTable from "./SelectSupplierTable";
+import { TrashFill, Plus } from "react-bootstrap-icons";
+import { useEffect, useState } from "react";
 
 const TotalOrderTable = ({
   listDetailTotal,
+  setListDetailTotal,
+  selectedSupplier,
+  setSelectedSupplier,
   BagiPesananHandler,
-  selectedHasilPembagian,
-  setSelectedHasilPembagian,
   detailSupplier,
   loadingNewSupplier,
 }) => {
-  const tempData = [...listDetailTotal];
-  const TotalOrderList = () => {
-    return listDetailTotal.map((detailTotal, index) => {
-      return (
-        <tr key={index}>
-          <td>{index + 1}</td>
-          <td>{detailTotal.id_produk.name}</td>
-          <td>{detailTotal.jumlah_barang}</td>
-          <td>{detailTotal.unit}</td>
-        </tr>
-      );
+  useEffect(() => {
+    const supplier = listDetailTotal.map((detail) => {
+      return [
+        {
+          id_supplier: { id: false, name: false },
+          id_produk: detail.id_produk,
+          unit: detail.unit,
+          jumlah: 0,
+        },
+      ];
     });
+    console;
+    setSelectedSupplier(supplier);
+  }, [listDetailTotal]);
+
+  const AddSupplier = (detailIndex) => {
+    const result = listDetailTotal.map((detail, index) => {
+      if (detailIndex === index) {
+        console.log("ADDD?");
+        setSelectedSupplier((prevState) => {
+          prevState[detailIndex].push({
+            id_supplier: { id: false, name: false },
+            id_produk: detail.id_produk,
+            unit: detail.unit,
+            jumlah: 0,
+          });
+          return prevState;
+        });
+      }
+      return detail;
+    });
+
+    setListDetailTotal(result);
+
+    console.log("SELECTEDSUP", selectedSupplier);
   };
 
-  if (listDetailTotal.length > 0) {
+  const DeleteSelectedSupplier = (supplierIndex, detailIndex) => {
+    const temp = listDetailTotal.map((detail, index) => {
+      if (index === detailIndex) {
+        detail.supplier.splice(supplierIndex, 1);
+      }
+      return detail;
+    });
+
+    setListDetailTotal(temp);
+  };
+
+  const SupplierList = ({
+    supplierList,
+    supplier,
+    supplierIndex,
+    detailIndex,
+  }) => {
+    return (
+      <td style={{ minHeight: "10rem" }}>
+        <div>
+          <div style={{ display: "flex" }}>
+            <select
+              style={{ minWidth: "7rem" }}
+              value={JSON.stringify(supplier["id_supplier"])}
+              onChange={(e) => {
+                supplier["id_supplier"] = e.target.value;
+                setListDetailTotal(listDetailTotal);
+              }}
+            >
+              <option value={{ id: false, name: false }}>-</option>
+              {detailSupplier &&
+                detailSupplier.map((supplier) => {
+                  return (
+                    <option
+                      value={supplier.id_supplier}
+                      key={supplier.id_supplier.id}
+                    >
+                      {supplier.id_supplier.name}
+                    </option>
+                  );
+                })}
+            </select>
+            <input
+              type="number"
+              placeholder={`Jumlah barang... ${supplierIndex}`}
+              onChange={(e) => {
+                setSelectedSupplier((prevState) => {
+                  prevState[detailIndex][supplierIndex].jumlah = e.target.value;
+                  return prevState;
+                });
+
+                console.log("selecsup", selectedSupplier);
+              }}
+              value={supplier.jumlah}
+            />
+            {supplierList?.length > 1 && (
+              <button
+                className="btn btn-danger"
+                onClick={() =>
+                  DeleteSelectedSupplier(supplier, supplierIndex, detailIndex)
+                }
+              >
+                <TrashFill />
+              </button>
+            )}
+
+            <button
+              className="btn btn-primary "
+              onClick={() => {
+                AddSupplier(detailIndex);
+              }}
+            >
+              <Plus />
+            </button>
+          </div>
+        </div>
+      </td>
+    );
+  };
+
+  const TotalOrderList = () => {
+    if (listDetailTotal)
+      return listDetailTotal.map((detailTotal, index) => {
+        return (
+          <tr key={index}>
+            <td>{index + 1}</td>
+            <td>{detailTotal?.id_produk.name}</td>
+            <td>{detailTotal?.jumlah_barang}</td>
+            <td>{detailTotal?.unit}</td>
+            {selectedSupplier[index]?.map((supplier, supplierIndex) => {
+              console.log("SUPPLIER", supplierIndex);
+              return (
+                <SupplierList
+                  key={supplierIndex}
+                  supplierList={selectedSupplier}
+                  supplier={supplier}
+                  supplierIndex={supplierIndex}
+                  detailIndex={index}
+                />
+              );
+            })}
+          </tr>
+        );
+      });
+  };
+
+  if (listDetailTotal?.length > 0) {
     return (
       <div>
         <div>
@@ -42,13 +173,6 @@ const TotalOrderTable = ({
                 <TotalOrderList />
               </tbody>
             </table>
-            <SelectSupplierTable
-              selectedHasilPembagian={selectedHasilPembagian}
-              setSelectedHasilPembagian={setSelectedHasilPembagian}
-              listDetailTotal={listDetailTotal}
-              detailSupplier={detailSupplier}
-              loadingNewSupplier={loadingNewSupplier}
-            />
           </div>
           <button onClick={BagiPesananHandler}>Bagi pesanan</button>
         </div>
